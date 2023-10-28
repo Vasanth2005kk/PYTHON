00c7af90e800ef904bc26ea30f586b911f8f4089
Graceglor <139038075+Graceglor@users.noreply.github.com>
2023-10-28 12:10:48 +0530

    Update #09_03.py

--- Error Makes Clever Academy/#09_03.py
+++ Error Makes Clever Academy/#09_03.py
@@ -1,9 +1,9 @@
-'''print even number between1 to10.
+'''print odd number between1 to10.
 
-sample output:
-2,4,6,8,10
+sample output
+1,3,5,7,9,
 
 '''
 for i in range(1,10+1):
-    if i%2==0:
+    if i%2==1:
         print(i,end=",")
