cfabf28346b7e74eb0709a7080f5885a90ec9eec
Vasanth2005kk <vasanth2005kk@gmail.com>
2024-03-02 18:51:19 +0530

    update in day_11.py python file

--- instgram/day_challanges/day_11.py
+++ instgram/day_challanges/day_11.py
@@ -7,3 +7,21 @@ if target in array:
     print("Element found index :",array.index(target))
 else:
     print("Element not found")
+
+'''
+#list_input=map(int,input().strip("[]").split(","))
+
+a=[1,2,3,4,5,6,7]
+target=7
+
+count=0
+if target in a:
+    for i in a:
+        if i == target:
+            break
+        else:
+            count+=1
+    print("element found index:",count)
+else:
+    print("Elenment not found ")
+'''
