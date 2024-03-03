1e89dfe9c22650808993f97a99a08813619a52d0
Vasanth2005kk <vasanth2005kk@gmail.com>
2024-03-03 15:19:23 +0530

    day_19.py

---
+++ instgram/day_challanges/day_19.py
@@ -0,0 +1,18 @@
+def solve(s):
+   res = ""
+   cnt = 1
+   for i in range(1, len(s)):
+      if s[i - 1] == s[i]:
+         cnt += 1
+      else:
+         res = res + s[i - 1]
+         if cnt > 1:
+            res += str(cnt)
+         cnt = 1
+   res = res + s[-1]
+   if cnt > 1:
+      res += str(cnt)
+   return res
+
+s = "abcc"
+print(solve(s))
